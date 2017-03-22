d337096b1f7ee702a137a6b060e2433d878e8657
nibie <nico.cambelisa@gmail.com>
2017-03-22 21:06:50 +0800

    delete alll

--- application/config/app_constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-/* Database Table Constants */
-define('USERS','prj_user_aims');
-define('USER_GROUPS','prj_user_groups_aims');
-define('USER_GROUP_REFERENCE','prj_user_group_reference_aims');
